e2f8e7ecbb8dd4b7e17fd4ae044fc69e8e5d0085
Liam Koshrovski <liamk@campus.technion.ac.il>
2023-06-01 14:50:30 +0000

    Implemented ili_utils.c

--- part2/ili_utils.c
+++ part2/ili_utils.c
@@ -1,25 +1,45 @@
 #include <asm/desc.h>
 
 void my_store_idt(struct desc_ptr *idtr) {
-// <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
-// </STUDENT FILL>
+    asm volatile("sidt %0"
+                :"=m" (*idtr)
+                :
+                :
+    );
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
-// <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
-// <STUDENT FILL>
+    asm volatile("lidt %0"
+                :
+                :"m" (*idtr)
+                :
+    );
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
+    unsigned short low=addr;
+    unsigned short middle=addr>>16;
+    unsigned int high=addr>>32;
 
-// </STUDENT FILL>
+    gate->offset_low=low;
+    gate->offset_middle=middle;
+    gate->offset_high=high;
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
+    unsigned long low, middle, high, result=0;
+    low=gate->offset_low;
+    low=low & (0xffff);
 
-// </STUDENT FILL>
+    middle=gate->offset_middle;
+    middle=middle & (0xffff);
+
+    high=gate->offset_high;
+    high=high & (0xffffffff);
+
+    middle=middle<<16;
+    high=high<<32;
+
+    result=low+middle+high;
+    return result;
 }
